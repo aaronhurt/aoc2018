d802a2f8ff76583161d7ef892f0eb7fa00fd1b42
Aaron Hurt <ahurt@ena.com>
2018-12-03 18:28:03 -0600

    day 2...

---
+++ day2/day2.tcl
@@ -0,0 +1,69 @@
+#!/usr/bin/env tclsh
+#
+## Advent of Code 2018 - Day 2
+#
+
+source "../common.tcl"
+
+namespace eval ::dayTwo {}
+
+proc ::dayTwo::countChars {string} {
+	array set ret [list 2 0 3 0]
+	set splitString [split $string ""]
+	set charList [lsort -unique [split $string ""]]
+
+	if {[llength $splitString] == [llength $charList]} {
+		return [array get ret]
+	}
+
+	foreach c $charList {
+		if {[set count [llength [lsearch -all -inline -exact $splitString $c]]]} {
+			if {$count == 2 || $count == 3} {
+				set ret($count) 1
+			}
+		}
+	}
+
+	return [array get ret]
+}
+
+proc ::dayTwo::partOne {} {
+	array set counts [list 2 0 3 0]
+
+	foreach line [::common::input] {
+		array set lc [::dayTwo::countChars $line]
+		set counts(2) [expr $counts(2) + $lc(2)]
+		set counts(3) [expr $counts(3) + $lc(3)]
+	}
+	::common::log "PartOne: [expr $counts(2) * $counts(3)]"
+}
+
+
+proc ::dayTwo::partTwo {} {
+	set lines [::common::input]
+	array set sims [list]
+
+	foreach aLine $lines {
+		set aSplit [split $aLine ""]
+		foreach bLine $lines {
+			set dc 0
+			set idx 0
+			array set sims [list $aLine [list]]
+			foreach aChar $aSplit bChar [split $bLine ""] {
+				if {[string compare $aChar $bChar] != 0} {
+					set dc [expr $dc + 1]
+				} else {
+					array set sims [list $aLine [linsert $sims($aLine) $idx $aChar]]
+				}
+				set idx [expr $idx + 1]
+			}
+			if {$dc == 1} {
+				::common::log "PartTwo: [join $sims($aLine) ""]"
+				return
+			}
+		}
+	}
+}
+
+::dayTwo::partOne
+::dayTwo::partTwo
